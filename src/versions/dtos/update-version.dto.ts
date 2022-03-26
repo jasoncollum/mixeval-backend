77e1f2614af8e0cca3647360f7037cc9033ce60e
Jason Collum <jasoncollum@gmail.com>
2022-03-26 12:31:52 -0500

    adds versions.subscriber

--- src/versions/dtos/update-version.dto.ts
+++ src/versions/dtos/update-version.dto.ts
@@ -1,6 +1,9 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class UpdateVersionDto {
   @IsNumber()
   number: number;
+
+  @IsString()
+  songId: string;
 }
