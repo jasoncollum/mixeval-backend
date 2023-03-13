6c39e219f84d53c313e32e84ce877eadd9a78b60
Jason Collum <jasoncollum@gmail.com>
2023-03-13 15:54:09 -0500

    adds aritstId and artist entity to update-song.dto, refactors takes-artistId-returns-artist.pipe to implement either create-song.dto or update-song.dto

--- src/songs/dtos/update-song.dto.ts
+++ src/songs/dtos/update-song.dto.ts
@@ -1,9 +1,16 @@
 import { IsBoolean, IsOptional, IsString } from 'class-validator';
+import { Artist } from 'src/artists/artist.entity';
 
 export class UpdateSongDto {
   @IsString()
   @IsOptional()
   title: string;
+
+  @IsString()
+  @IsOptional()
+  artistId: string;
+
+  artist: Artist;
 
   @IsBoolean()
   @IsOptional()
