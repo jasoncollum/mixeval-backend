dc77830c84dc40b071d6ab4ab2e62f3209ebc141
Jason Collum <jasoncollum@gmail.com>
2023-04-05 17:35:43 -0500

    clarifies password requirements at signup

--- src/auth/dtos/auth-signup-credentials.dto.ts
+++ src/auth/dtos/auth-signup-credentials.dto.ts
@@ -16,12 +16,12 @@ export class AuthSignupCredentialsDto {
   email: string;
 
   // Must contain 1 uppercase letter, 1 lowercase letter, 1 number or special character
-  // There is no length validation in this regex
   @IsString()
   @MinLength(8)
   @MaxLength(32)
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: 'Password is too weak',
+    message:
+      'password must contain 1 uppercase letter, 1 lowercase letter, 1 number or special character',
   })
   password: string;
 }
